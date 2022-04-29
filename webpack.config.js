8baeae607e6e6b1e7e01776cc4e48eee281f8f36
yoaoses <yoa.oses@gmail.com>
2022-04-28 20:14:01 -0500

    Login Layout

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports={
                 ]
             },
             {
-                test: /\.s[ac]ss$/i,
+                test: /\.(css|scss)$/,
                 use:[
                         "style-loader",
                         "css-loader",
@@ -53,6 +53,6 @@ module.exports={
             directory: path.join(__dirname, 'dist')
         },
         compress: true,
-        port: 3005,
+        port: 3000,
     }
 }
