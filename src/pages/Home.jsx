cbc0b927726a16bdec0f3f83650758faf5c35e46
yoaoses <yoa.oses@gmail.com>
2022-04-29 19:03:26 -0500

    transformacion de htm a componentes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import Header from '../components/Header';
+import ProductList from '../containers/ProductList';
 
 const Home =()=>{
     return(
-        <Header />
+        <ProductList />
+        
     );
 }
 
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,25 +1,44 @@
+
+					{/* <Routes>
+						<Route path='/' element={<Home />} />
+						<Route path='/login' element={<Login />} />
+						<Route path='/recovery-password' element={<RecoveryPassword />}/>
+						<Route path='*' element={<NotFound />} />
+					</Routes> */}
+
 import React from 'react';
-import { BrowserRouter, Routes, Route } from'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from '../containers/Layout';
-import Login from '../containers/Login';
-import RecoveryPassword from '../containers/RecoveryPassword';
 import Home from '../pages/Home';
+import Login from '../pages/Login';
+import PasswordRecovery from '../pages/PasswordRecovery';
+import SendEmail from '../pages/SendEmail';
+import NewPassword from '../pages/NewPassword';
+import MyAccount from '../pages/MyAccount';
+import CreateAccount from '../pages/CreateAccount';
+import Checkout from '../pages/Checkout';
+import Orders from '../pages/Orders';
 import NotFound from '../pages/NotFound';
 import '../Styles/global.scss';
 
 const App = () => {
 	return (
 		<BrowserRouter>
-				<Layout>
-					<Routes>
-						<Route path='/' element={<Home />} />
-						<Route path='/login' element={<Login />} />
-						<Route path='/recovery-password' element={<RecoveryPassword />}/>
-						<Route path='*' element={<NotFound />} />
-					</Routes>
-				</Layout>
+			<Layout>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/password-recovery" element={<PasswordRecovery />} />
+					<Route path="/send-email" element={<SendEmail />} />
+					<Route path="/new-password" element={<NewPassword />} />
+					<Route path="/account" element={<MyAccount />} />
+					<Route path="/signup" element={<CreateAccount />} />
+					<Route path="/checkout" element={<Checkout />} />
+					<Route path="/orders" element={<Orders />} />
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			</Layout>
 		</BrowserRouter>
-		
 	);
 }
 
